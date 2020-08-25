6bb7b4b4e02dec617611dbc9ef0f2b3f052bb0a9
pqc1996 <a719323690>
2020-08-25 10:23:51 +0800

    20200823

---
+++ java/Arrays/m01_07.java
@@ -0,0 +1,63 @@
+package Arrays;
+//给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+//
+// 不占用额外内存空间能否做到？
+//
+//
+//
+// 示例 1:
+//
+// 给定 matrix =
+//[
+//  [1,2,3],
+//  [4,5,6],
+//  [7,8,9]
+//],
+//
+//原地旋转输入矩阵，使其变为:
+//[
+//  [7,4,1],
+//  [8,5,2],
+//  [9,6,3]
+//]
+//
+//
+// 示例 2:
+//
+// 给定 matrix =
+//[
+//  [ 5, 1, 9,11],
+//  [ 2, 4, 8,10],
+//  [13, 3, 6, 7],
+//  [15,14,12,16]
+//],
+//
+//原地旋转输入矩阵，使其变为:
+//[
+//  [15,13, 2, 5],
+//  [14, 3, 4, 1],
+//  [12, 6, 8, 9],
+//  [16, 7,10,11]
+//]
+//
+// Related Topics 数组
+// 👍 118 👎 0
+public class m01_07 {
+    public void rotate(int[][] matrix) {
+        int temp = 0;
+        for(int i = 0;i<matrix.length-1;i++){
+            for(int j = i+1;j<matrix[0].length;j++){
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i = 0;i<=matrix.length-1;i++){
+            for(int j = 0;j<matrix[0].length/2;j++){
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
+            }
+        }
+    }
+}
